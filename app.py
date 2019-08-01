46aff5648a3082f5ca882f53cc4246597a8a0294
myname21-meet <myname21@meet.mit.edu>
2019-08-01 05:16:22 -0400

    aaa

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+# from databases import *
+from flask import Flask, request, redirect, render_template
+from flask import session as login_session
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World test!'
+    return render_template('personalproject.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
